b37b2bd00b31645c3732593fb5f1bc50fa232c30
DSRschool <114143953+mnadalces@users.noreply.github.com>
2023-08-11 17:11:29 +0200

    02_06Creando una tarea (paso 3)

---
+++ EjemploHolaMundo/src/test/java/HolaMundoTest.java
@@ -0,0 +1,45 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+class HolaMundoTest {
+	@Test
+	public void testHolaMundoCasi() {
+		// salvaguarda
+		PrintStream originalOut = System.out;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(bos));
+
+		// acción
+		HolaMundo.main(new String[] {});
+		String salida = bos.toString();
+		
+		// pruebas
+		assertTrue(salida.toUpperCase().contains("HOLA"));
+		assertTrue(salida.toUpperCase().contains("MUNDO"));
+
+		// restauración
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void testHolaMundoExacto() {
+		// salvaguarda
+		PrintStream originalOut = System.out;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(bos));
+
+		// acción
+		HolaMundo.main(new String[] {});
+
+		// pruebas
+		assertEquals("Hola mundo!" + System.lineSeparator(), bos.toString());
+
+		// restauración
+		System.setOut(originalOut);
+	}
+}
